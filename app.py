f3f504c729005343a8da37365db91ec6561e978b
Anil Yelin <anil.yelin@dwpbank.de>
2024-09-02 16:42:17 +0200

    using columns for inputs

--- app.py
+++ app.py
@@ -5,6 +5,17 @@ import numpy as np
 import pandas as pd
 
 def calculateSMA(df, k):
+    """
+    Calculates the Simple Moving Average (SMA) for a given dataframe
+
+    Parameters:
+        df      : Dataframe with the financial data for a given stock asset
+        k (int) : Represents the number of days for the calculation of SMA
+
+    Returns:
+        result_df : Returns a df with the calculated SMA with a given k 
+                    which will be used for plotting a graph
+    """
     logging = True
     results = []
     i=0
@@ -21,49 +32,28 @@ def calculateSMA(df, k):
     print()
     x_data = df["Date"][tmp-1:len(df["Date"])]
     result_df = pd.DataFrame(list(zip(x_data, results)), columns=["foo","bar"])
-    #result_df = pd.DataFrame(list(zip([1,2,3],[4,-1,60])), columns=["a","b"])
     print(result_df)
     return result_df
 
 st.title("Stock Analysis Dashboard")
 ticker = st.text_input("Enter stock ticker symbol: ", "AAPL")
-time_interval = st.radio("Select time interval", ["1d", "5d", "1mo", "3mo"])
+time_interval_col, sma_selection = st.columns([1,1])
+time_interval_col.subheader("Timeinterval Selector")
+time_interval_value = time_interval_col.radio("Select time interval", ["1d", "5d", "1mo", "3mo"])
+sma_selection.subheader("SMA Day Selector")
+sma_selection_value = sma_selection.number_input("Enter SMA day parameter", min_value=1, max_value=30)
+
 if st.button("Submit"):
     with st.spinner("Fetching Data"):
         stock_data = yf.Ticker(ticker)
-        stock_data_history = stock_data.history(period=time_interval)
+        stock_data_history = stock_data.history(period=time_interval_value)
         st.write(stock_data_history)
         stock_data_history.reset_index(inplace=True)
-        st.subheader("{} Chart for {}".format(time_interval, ticker))
+        st.subheader("{} Chart for {}".format(time_interval_value, ticker))
         st.line_chart(stock_data_history, x='Date', y='Close', color="#a338eb")
 
-        st.header("Simple Moving Average")
+        st.header("Simple Moving Average for {}".format(sma_selection_value))
         df = stock_data_history[['Date','Close']].copy()
         calculateSMA(df,4)
-        #days = st.number_input("Enter days", min_value=5, step=1)
-        st.line_chart(calculateSMA(df, 4).set_index('foo'))
+        st.line_chart(calculateSMA(df, sma_selection_value).set_index('foo'))
 
-        
-        
-        #calculateSMA()
-        #col1, col2 = st.columns([1, 1])
-        #data = np.random.randn(10, 1)
-        
-
-        #col1.subheader("A wide column with a chart")
-        #col1.line_chart(data)
-
-        #col2.subheader("A narrow column with a chart")
-        #col2.line_chart(data)
-
-
-
-
-data = {
-        "calories": [420, 380, 390, 370, 360, 350, 340],
-        "duration": [50, 40, 45, 41, 42, 45, 32]
-    }
-a = pd.DataFrame(data)
-        
-#df = calculateSMA(a,4)
-#st.line_chart(df.set_index('a'))
